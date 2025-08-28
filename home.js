2caeafa91e31434bec58b6e7ed8a6759e3762426
romanakhatun <romanakhatun2004@gmail.com>
2025-08-28 12:10:41 +0600

    done payoo

--- home.js
+++ home.js
@@ -1,4 +1,111 @@
-// toggle
+const transactionData = [];
+const defaultPin = 1234;
+
+// reuseable function
+function getValueToNumber(id) {
+  return parseInt(document.getElementById(id).value);
+}
+
+// add money
+document
+  .getElementById("add-money-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    const bankAccount = document.getElementById("bank-account").value;
+    const accountNumber = document.getElementById("number").value;
+    const pin = getValueToNumber("add-pin");
+    const addAmount = getValueToNumber("add-amount");
+    const balance = document.getElementById("balance");
+
+    if (accountNumber.length < 11) {
+      alert("please provide valid account number");
+      return;
+    }
+    if (pin !== defaultPin) {
+      alert("please prove valid pin number");
+      return;
+    }
+    if (addAmount <= 0) {
+      alert("invalid amount");
+      return;
+    }
+    const newBalance = addAmount + parseInt(balance.innerText);
+    balance.innerText = newBalance;
+
+    const data = {
+      name: "add money",
+      date: new Date().toLocaleTimeString(),
+    };
+    transactionData.push(data);
+    console.log(transactionData);
+  });
+
+// cash out money
+document.getElementById("withdraw-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  const withdrawNum = document.getElementById("withdraw-number").value;
+  const withdrawPin = getValueToNumber("withdraw-pin");
+  const withdrawAmount = getValueToNumber("withdraw-amount");
+  const balance = document.getElementById("balance");
+
+  if (withdrawNum.length < 11) {
+    alert("please provide valid account number");
+    return;
+  }
+
+  if (withdrawPin !== defaultPin) {
+    alert("please provide valid pin number");
+    return;
+  }
+  if (withdrawAmount <= 0 || withdrawAmount > balance.innerText) {
+    alert("invalid amount");
+    return;
+  }
+  const newBalance = parseInt(balance.innerText) - withdrawAmount;
+  balance.innerText = newBalance;
+
+  const data = {
+    name: "Cash out",
+    date: new Date().toLocaleTimeString(),
+  };
+
+  transactionData.push(data);
+  console.log(transactionData);
+});
+
+document
+  .getElementById("transaction-menu")
+  .addEventListener("click", function () {
+    const transactionContainer = document.getElementById(
+      "transaction-container"
+    );
+    transactionContainer.innerText = "";
+
+    for (const data of transactionData) {
+      const parentDiv = document.createElement("div");
+      parentDiv.innerHTML = `
+    <div
+            class="flex items-center justify-between rounded-xl border border-[#0808081a] px-4 py-3 bg-white"
+          >
+            <div class="flex items-center gap-2">
+              <div class="bg-[#f4f5f7] rounded-full p-3">
+                <img src="./assets/bill.png" alt="" />
+              </div>
+              <div class="text-[#080808b3]">
+                <h3 class="font-semibold">${data.name}</h3>
+                <p class="text-xs">${data.date}</p>
+              </div>
+            </div>
+            <div>
+              <i class="fa-solid fa-ellipsis-vertical"></i>
+            </div>
+          </div>
+    `;
+      transactionContainer.appendChild(parentDiv);
+    }
+  });
+
+// toggle reuseable function
 function handleToggle(id) {
   const forms = document.getElementsByClassName("form");
 
@@ -7,7 +114,7 @@ function handleToggle(id) {
   }
   document.getElementById(id).style.display = "block";
 }
-
+// focus button style change
 function handleToggleButton(id) {
   const menuBtns = document.getElementsByClassName("menu-btn");
 
@@ -26,7 +133,7 @@ function handleToggleButton(id) {
     .getElementById(id)
     .classList.add("border-[#0874F2]", "text-[#0874F2]", "bg-[#0874f20d]");
 }
-////
+// toggle system
 document.getElementById("money-menu").addEventListener("click", function () {
   handleToggle("money-form");
   handleToggleButton("money-menu");
@@ -34,70 +141,12 @@ document.getElementById("money-menu").addEventListener("click", function () {
 
 document.getElementById("cashOut-menu").addEventListener("click", function () {
   handleToggle("cashOut-form");
-
   handleToggleButton("cashOut-menu");
 });
 
-const defaultPin = 1234;
-// reuseable function
-function getValueToNumber(id) {
-  return parseInt(document.getElementById(id).value);
-}
-
-function getInnerText(id) {
-  return parseInt(document.getElementById(id).innerText);
-}
-
-// add money
 document
-  .getElementById("add-money-btn")
-  .addEventListener("click", function (e) {
-    e.preventDefault();
-
-    const bankAccount = document.getElementById("bank-account").value;
-    const accountNumber = document.getElementById("number").value;
-    const pin = getValueToNumber("add-pin");
-
-    // add amount
-    const addAmount = getValueToNumber("add-amount");
-    const balance = document.getElementById("balance");
-
-    if (accountNumber.length < 11) {
-      alert("please provide valid account number");
-      return;
-    }
-    if (pin !== defaultPin) {
-      alert("please prove valid pin number");
-      return;
-    }
-
-    const newBalance = addAmount + parseInt(balance.innerText);
-    balance.innerText = newBalance;
-    console.log("add money successfully");
+  .getElementById("transaction-menu")
+  .addEventListener("click", function () {
+    handleToggle("transaction-form");
+    handleToggleButton("transaction-menu");
   });
-
-// cash out money
-document.getElementById("withdraw-btn").addEventListener("click", function (e) {
-  e.preventDefault();
-
-  const withdrawNum = document.getElementById("withdraw-number").value;
-  const withdrawPin = getValueToNumber("withdraw-pin");
-
-  const withdrawAmount = getValueToNumber("withdraw-amount");
-
-  const balance = document.getElementById("balance");
-
-  if (withdrawNum.length < 11) {
-    alert("please provide valid account number");
-    return;
-  }
-
-  if (withdrawPin !== defaultPin) {
-    alert("please provide valid pin number");
-    return;
-  }
-
-  const newBalance = parseInt(balance.innerText) - withdrawAmount;
-  balance.innerText = newBalance;
-  console.log("remove money successfully");
-});
